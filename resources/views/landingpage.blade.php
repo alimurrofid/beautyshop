cc1481751c3a08125bdd4f3b0e30690ee9f8a4ab
alimurrofid <alimurrofid77@gmail.com>
2023-12-28 20:16:47 +0700

    feat:landingpage

--- resources/views/landingpage.blade.php
+++ resources/views/landingpage.blade.php
@@ -1,98 +1,54 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Beauty Shop</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
-</head>
-
-<body>
-
-
-    <nav class="bg-white border-gray-200 dark:bg-gray-900">
-        <div class="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-            <a href="https://flowbite.com/" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img src="https://flowbite.com/docs/images/logo.svg" class="h-8" alt="Flowbite Logo" />
-                <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">Flowbite</span>
-            </a>
-            <div class="flex items-center md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                <button type="button"
-                    class="flex text-sm bg-gray-800 rounded-full md:me-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
-                    id="user-menu-button" aria-expanded="false" data-dropdown-toggle="user-dropdown"
-                    data-dropdown-placement="bottom">
-                    <span class="sr-only">Open user menu</span>
-                    <img class="w-8 h-8 rounded-full" src="/docs/images/people/profile-picture-3.jpg" alt="user photo">
-                </button>
-                <!-- Dropdown menu -->
-                <div class="z-50 hidden my-4 text-base list-none bg-white divide-y divide-gray-100 rounded-lg shadow dark:bg-gray-700 dark:divide-gray-600"
-                    id="user-dropdown">
-                    <div class="px-4 py-3">
-                        <span class="block text-sm text-gray-900 dark:text-white">Bonnie Green</span>
-                        <span class="block text-sm  text-gray-500 truncate dark:text-gray-400">name@flowbite.com</span>
-                    </div>
-                    <ul class="py-2" aria-labelledby="user-menu-button">
-                        <li>
-                            <a href="#"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Dashboard</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Settings</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Earnings</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                class="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100 dark:hover:bg-gray-600 dark:text-gray-200 dark:hover:text-white">Sign
-                                out</a>
-                        </li>
-                    </ul>
+@extends('user.layouts.usermain')
+@section('title', 'Landingpage')
+@section('usercontent')
+    <!-- Hero Section -->
+    <section id="home" class="pt-10">
+        <div class="container lg:px-12">
+            <div class="flex flex-wrap">
+                <div class="self-center w-full px-4 md:w-1/2 md:ps-10 lg:ps-4">
+                    <h1 class="mb-5 text-3xl font-bold uppercase text-neutral-700 lg:text-4xl">
+                        Discover your inner beauty with our products
+                    </h1>
+                    <p class="mb-5 text-lg text-neutral-700 lg:text-2xl lg:mb-10">Great gift for yourself and loved ones</p>
+                    <a href=""
+                        class="px-8 py-2 text-lg font-semibold text-white rounded-lg bg-rose-300 lg:px-14 lg:py-3 lg:text-xl hover:bg-rose-400">Shop
+                        Now</a>
                 </div>
-                <button data-collapse-toggle="navbar-user" type="button"
-                    class="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-                    aria-controls="navbar-user" aria-expanded="false">
-                    <span class="sr-only">Open main menu</span>
-                    <svg class="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none"
-                        viewBox="0 0 17 14">
-                        <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M1 1h15M1 7h15M1 13h15" />
-                    </svg>
-                </button>
-            </div>
-            <div class="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-user">
-                <ul
-                    class="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-                    <li>
-                        <a href="#"
-                            class="block py-2 px-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-                            aria-current="page">Home</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Pricing</a>
-                    </li>
-                    <li>
-                        <a href="#"
-                            class="block py-2 px-3 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-                    </li>
-                </ul>
+                <div class="self-end w-full px-8 md:w-1/2">
+                    <div class="mt-10">
+                        <img src="{{ asset('assets/images/hero.png') }}" alt="hero" class="max-w-full mx-auto">
+                    </div>
+                </div>
             </div>
         </div>
-    </nav>
+    </section>
+    <!-- End Hero Section -->
 
-</body>
-
-</html>
+    <!-- Product Section -->
+    <section id="product" class="pt-16 pb-24">
+        <div class="container mx-auto">
+            <div class="w-full px-4 ">
+                <div class="text-center ">
+                    <h2 class="mb-12 text-xl font-bold uppercase text-slate-700">
+                        New Arrivals
+                    </h2>
+                    <div class="flex flex-wrap items-center justify-center gap-7 mt-7">
+                        <!-- Card Candidates -->
+                        <div class="max-w-sm bg-white rounded-md shadow w-72">
+                            <img class="object-cover object-center w-full rounded-t-md h-72"
+                                src="{{ asset('assets/images/skincare.png') }}" alt="" />
+                            <div class="flex flex-col items-start mt-2 ms-3">
+                                <span class="my-1 text-xl font-semibold text-black">Dewy Glow Jelly Cream</span>
+                                <span class="mt-1 mb-4 text-base font-medium text-slate-600">Rp. 20.000,00</span>
+                            </div>
+                            <button class="px-20 py-1 mx-auto mb-6 font-bold border border-black rounded-lg hover:text-rose-400 hover:border-rose-400 hover:shadow-md">Add To
+                                Cart</button>
+                        </div>
+                        <!-- End Card Candidates -->
+                        
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+@endsection
